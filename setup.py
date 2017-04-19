014ffce8ed89a7b762b375719891db71505a1acd
Andros Fenollosa <andros@fenollosa.email>
2017-04-20 00:57:59 +0200

    Update icon and Add info

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import setup
+
+OPTIONS = {
+    'iconfile': 'icon.icns'
+    }
+DATA_FILES = ['guetzli-osx']
 setup(
-    app=["main.py"],
+    app=["Guetzli-R.py"],
+    options={'py2app': OPTIONS},
+    data_files=DATA_FILES,
     setup_requires=["py2app"],
 )
